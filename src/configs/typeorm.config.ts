1146cdced99bd2555d0cf65e4ac2003d413faa56
Jessé Satlei <jesse.satlei@ecenture.com.br>
2022-08-24 13:26:45 -0300

    test

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,9 +1,8 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { Movie } from 'src/modules/movies/entities/movie.entity';
 import { User } from 'src/modules/users/entities/user.entity';
-require('dotenv').config()
 
-const database_url = process.env.DATABASE_URL || '';
+const database_url = 'postgres://peurwpubzsysyc:045b3b45e588d2b35a14bf06e22e302c4ff89c92c5c14b162e4db6f01e5af907@ec2-34-227-135-211.compute-1.amazonaws.com:5432/d1ogth0sehofqe';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
     url: database_url,
